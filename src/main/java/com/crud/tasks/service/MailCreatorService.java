6caea084a7ccbdeeec0e5ad12142dda11f6322b3
Kubecko <MKubecko@gmail.com>
2018-10-11 02:01:52 +0200

    24.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -20,7 +20,8 @@ public class MailCreatorService {
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("task_url","http://localhost:8888/crud" );
+        context.setVariable("task_url","http://localhost:8888/tasks_frontend/" );
+        context.setVariable("email_adress", "MKodersik@gmail.com");
         context.setVariable("button","Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
         return templateEngine.process("mail/created-trello-card-mail",context );
